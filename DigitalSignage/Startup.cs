01065743e91bba0b864ddd5250843e1c3ad37bd0
alphanitinjoshi <nitin.joshi@alpha-ict.in>
2020-09-24 14:43:21 +0530

    Add project files.

---
+++ DigitalSignage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DigitalSignage.Startup))]
+namespace DigitalSignage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
